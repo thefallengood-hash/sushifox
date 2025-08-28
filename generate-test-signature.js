46ad96cb28eaea2c3adbf48d7b5e5232b444e63b
thefallengood <thefallengood@gmail.com>
2025-08-28 04:00:32 +0300

    fix: create-order function safe JSON

--- generate-test-signature.js
+++ generate-test-signature.js
@@ -1,24 +1,14 @@
 // generate-test-signature.js
 import crypto from "crypto";
 
-// ⚠️ Вставь свой secretKey из WayForPay
-const secretKey = "4f8e577b3787070fc92079e227d37de997b1dd12";
-
-// Данные тестового платежа
+const SECRET_KEY = "4f8e577b3787070fc92079e227d37de997b1dd12";
 const merchantAccount = "freelance_user_68acde4a670e7";
 const orderReference = "TEST123";
 const amount = 100;
 const currency = "UAH";
 const transactionStatus = "Approved";
 
-// Собираем строку для подписи
-const signatureString = `${merchantAccount};${orderReference};${amount};${currency};${transactionStatus}`;
+const signatureString = [merchantAccount, orderReference, amount, currency, transactionStatus].join(";");
+const merchantSignature = crypto.createHmac("sha1", SECRET_KEY).update(signatureString).digest("base64");
 
-// Генерация подписи
-const signature = crypto
-  .createHmac("md5", secretKey)
-  .update(signatureString)
-  .digest("base64");
-
-console.log("Строка для подписи:", signatureString);
-console.log("Подпись:", signature);
+console.log("merchantSignature:", merchantSignature);
